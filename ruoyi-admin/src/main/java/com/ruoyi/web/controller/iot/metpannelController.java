fd48fa0e6661db4f606fdde2a5b8439445f8d046
zytzyt1 <2675213201@qq.com>
2019-12-20 12:25:18 +0800

    新建metpannel

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/iot/metpannelController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/iot/metpannelController.java
@@ -1,6 +1,11 @@
 package com.ruoyi.web.controller.iot;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/iot/metpannel")
 public class metpannelController {
     private String prefix="metpannel";
 
