1f187b57e124f94a8cf6fc281df246ffa85a90ad
liuyang73 <2395662915@qq.com>
2019-12-20 16:07:24 +0800

    修改milpannelController

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/iot/milpannelController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/iot/milpannelController.java
@@ -2,10 +2,16 @@ package com.ruoyi.web.controller.iot;
 
 import com.ruoyi.framework.web.base.BaseController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/iot/milpannel")
 public class milpannelController extends BaseController {
     private String prefix ="/iot/milpannel";
+
+    @GetMapping()
+    public String milpannel() {
+        return prefix + "/bMilpannel";
+    }
 }
