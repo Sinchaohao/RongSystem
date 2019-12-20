956e33679a2f8c5deeaa0cbf0fd72bf7f015bbd5
liuyang73 <2395662915@qq.com>
2019-12-20 16:00:53 +0800

    新建milpannelController

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/iot/milpannelController.java
@@ -0,0 +1,11 @@
+package com.ruoyi.web.controller.iot;
+
+import com.ruoyi.framework.web.base.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/iot/milpannel")
+public class milpannelController extends BaseController {
+    private String prefix ="/iot/milpannel";
+}
