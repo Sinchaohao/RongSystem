495c6fc9af46d7eeeee0842098b8cf0d715fe30d
Sinchaohao <Sinchaohao@outlook.com>
2019-12-26 11:10:09 +0800

    土壤、水质、能源的控制类和页面

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/iot/SoilPannelController.java
@@ -0,0 +1,26 @@
+package com.ruoyi.web.controller.iot;
+
+import com.ruoyi.framework.web.base.BaseController;
+import com.ruoyi.iot.service.IMetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @开发人员: 申超豪
+ * @开发单位：湖南农业大学物联网工程专业
+ * @Description:
+ * @Date:
+ */
+@Controller
+@RequestMapping("/iot/soilpannel")
+public class SoilPannelController extends BaseController {
+    private String prefix = "iot/soilpannel";
+
+
+    @GetMapping()
+    public String metpannel(){
+        return prefix + "/soilpannel";
+    }
+}
