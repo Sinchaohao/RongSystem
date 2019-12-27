cca5ada1a5b0023a360306a7f7aacad3736ef4fe
zytzyt1 <2675213201@qq.com>
2019-12-27 16:10:09 +0800

    在网页上显示下雨地区和下雨日期

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/iot/metpannelController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/iot/metpannelController.java
@@ -1,17 +1,45 @@
 package com.ruoyi.web.controller.iot;
 
 
+import com.ruoyi.common.base.BaseEntity;
+import com.ruoyi.common.page.TableDataInfo;
+import com.ruoyi.framework.web.base.BaseController;
+import com.ruoyi.iot.domain.MetCount;
+import com.ruoyi.iot.service.IMetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+import static com.github.pagehelper.page.PageMethod.startPage;
 
 @Controller
 @RequestMapping("/iot/metpannel")
-public class metpannelController {
-    private String prefix="metpannel";
+public class metpannelController extends BaseController {
+    private String prefix="iot/metpannel";
+
+
+    @Autowired
+    private IMetService metService;
+
 
     @GetMapping()
     public String metpannel(){
-        return prefix + "metpannel";
+        return prefix + "/metpannel";
+    }
+
+
+
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list()
+    {
+        startPage();
+        List<MetCount> list = metService.selectMet();
+        return getDataTable(list);
     }
 }
