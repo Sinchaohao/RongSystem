4a266462a5fc4f58fc156bda67cedbe5d92025ef
maoping9986 <2268638166@qq.com>
2019-12-29 14:33:52 +0800

    土壤数据采集设备信息查询相关代码

---
+++ sx-iot/src/main/java/com/ruoyi/iot/service/impl/SoilDeviceServiceImpl.java
@@ -0,0 +1,58 @@
+package com.ruoyi.iot.service.impl;
+
+import com.ruoyi.common.annotation.DataSource;
+import com.ruoyi.common.enums.DataSourceType;
+import com.ruoyi.iot.domain.SoilDevice;
+import com.ruoyi.iot.mapper.SoilDeviceMapper;
+import com.ruoyi.iot.service.SoilDeviceService;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+@Service
+public class SoilDeviceServiceImpl implements SoilDeviceService {
+    @Autowired
+    private SoilDeviceMapper soilDeviceMapper;
+    //    查询所有设备情况
+    @Override
+    @DataSource(value = DataSourceType.SXSOIL)
+    public List<SoilDevice> selectall(){
+       return soilDeviceMapper.selectall();
+    }
+    //    增加设备
+    @Override
+    @DataSource(value = DataSourceType.SXSOIL)
+    public int add(@Param("did") String did , @Param("dname") String dname , @Param("dstatus") int dstatus , @Param("dv") int dv
+            , @Param("dctime") Date dctime , @Param("dfame") Date dftime , @Param("dcname") String dcname , @Param("dfname") String dfname , @Param("note") String note){
+        return soilDeviceMapper.add(did, dname, dstatus, dv, dctime, dftime, dcname, dfname, note);
+    }
+
+    //    根据ID删除设备
+//    public int deletebyid(int did){
+//        return soilDeviceMapper.deletebyid(did);
+//        }
+
+    //    根据dname id删除设备记录
+    @Override
+    @DataSource(value = DataSourceType.SXSOIL)
+    public  int deletebyname(@Param("dname") String dname){
+        return soilDeviceMapper.deletebyname(dname);
+
+    }
+    //  根据ID 查询设备情况
+    @Override
+    @DataSource(value = DataSourceType.SXSOIL)
+    public SoilDevice selectbyid(String did)
+    {
+        return soilDeviceMapper.selectbyid(did);
+    }
+    //    根据地域名查询
+    @Override
+    @DataSource(value = DataSourceType.SXSOIL)
+    public  SoilDevice selectbyname( String dname)
+    {
+        return  soilDeviceMapper.selectbyname(dname);
+    }
+}
