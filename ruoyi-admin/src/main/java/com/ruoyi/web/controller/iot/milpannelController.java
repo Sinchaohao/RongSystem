c6adb955b9dd4972c47c2cb76e54c8f1558ce803
Sinchaohao <Sinchaohao@outlook.com>
2019-12-30 13:19:31 +0800

    update

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/iot/milpannelController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/iot/milpannelController.java
@@ -1,6 +1,5 @@
 package com.ruoyi.web.controller.iot;
 
-
 import com.ruoyi.framework.web.base.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
