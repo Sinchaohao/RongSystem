6b533dcc3f2593f9a7825a72bc284745fe3a2262
Sinchaohao <Sinchaohao@outlook.com>
2019-12-30 13:18:51 +0800

    广播数据中

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/iot/milpannelController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/iot/milpannelController.java
@@ -2,17 +2,9 @@ package com.ruoyi.web.controller.iot;
 
 import com.ruoyi.common.page.TableDataInfo;
 import com.ruoyi.framework.web.base.BaseController;
-import com.ruoyi.iot.domain.AreaData;
-import com.ruoyi.iot.domain.UserTerminals;
-import com.ruoyi.iot.service.IAreaDataService;
-import com.ruoyi.iot.service.IMilService;
-import com.ruoyi.iot.service.IUserTerService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
